71e49998b40f015bde9ac1e186e00430e73e84e6
SK-15 <61817515+SK-15@users.noreply.github.com>
2023-12-14 12:11:34 +0530

    Update naming.py

--- naming.py
+++ naming.py
@@ -11,7 +11,7 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to("cuda")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
 img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
 raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
